697bd49679c25636185a4e007977b3e88366b017
Sevgi Sancaktutan <sevgisancaktutan@gmail.com>
2022-11-09 11:25:34 +0300

    Musteri sinifi tamamlandı.

--- src/Musteri.java
+++ src/Musteri.java
@@ -1,2 +1,14 @@
-public class Musteri {
+public class Musteri extends Kullanici{
+
+    Sepet sepetim=new Sepet();
+    @Override
+    public void uruneBak(Urun urun) {
+    }
+    public void sepeteEkle(Urun urun){
+        sepetim.urunEkle(urun);
+    }
+    public Sepet getSepetim(){
+        return sepetim;
+    }
+
 }
