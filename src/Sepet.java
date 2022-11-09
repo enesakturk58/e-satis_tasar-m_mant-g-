5adf8f36e6e8784dc709b0fe75e404e47bc14544
Sevgi Sancaktutan <sevgisancaktutan@gmail.com>
2022-11-09 11:15:30 +0300

    Sepet Sinifi tamamlandi.

--- src/Sepet.java
+++ src/Sepet.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sepet {
+
+    List<Urun> urunler=new ArrayList<Urun>();
+    public double sepetHesapla(){
+        int toplamTutar=0;
+        for (Urun urun : urunler){
+            toplamTutar+=urun.getFiyat();
+        }
+        return toplamTutar;
+    }
+    public void urunEkle(Urun urun){
+        urunler.add(urun);
+    }
 }
