03e15e902c5d4ac9b469efcb772d6c6bbc47ed6d
Sevgi Sancaktutan <sevgisancaktutan@gmail.com>
2022-11-09 11:29:22 +0300

    Satici sinifi tamamlandı.

--- src/Satici.java
+++ src/Satici.java
@@ -1,2 +1,17 @@
-public class Satici {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Satici extends Kullanici{
+
+    List<Urun> urunler=new ArrayList<Urun>();
+    @Override
+    public void uruneBak(Urun urun) {
+
+    }
+    public void uruneEkle(Urun urun) {
+        urunler.add(urun);
+    }
+
+
+
 }
