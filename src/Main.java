338700867791dad95231750abb944b5b140fbd29
Enes Aktürk <enesakturk97@gmail.com>
2022-11-09 11:54:11 +0300

    kullanici adi ve maili degisti.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Musteri musteri1 = new Musteri();
+        Musteri musteri2 = new Musteri();
+        Urun urun1 = new Urun("Macbook pro",25000.0);
+        Urun urun2 = new Urun("Macbook air",19000.0);
+
+        musteri1.sepeteEkle(urun1);
+        musteri1.sepeteEkle(urun2);
+        musteri2.sepeteEkle(urun1);
+
+        System.out.println("Müsteri 1 sepet :  " + musteri1.getSepetim().sepetHesapla());
+        System.out.println("Müsteri 2 sepet :  " + musteri2.getSepetim().sepetHesapla());
+
+
     }
 }
