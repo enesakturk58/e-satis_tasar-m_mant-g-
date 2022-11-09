273d1a34d4ab9cd53b20b7e872c031717584d7e7
Sevgi Sancaktutan <sevgisancaktutan@gmail.com>
2022-11-09 11:10:25 +0300

    Urun Sinifi tamamlandi.

--- src/Urun.java
+++ src/Urun.java
@@ -1,2 +1,26 @@
 public class Urun {
+
+    String isim;
+    Double fiyat;
+
+    public Urun(String isim,Double fiyat){
+    setFiyat(fiyat);
+    setIsim(isim);
+    }
+
+    public Double getFiyat() {
+        return fiyat;
+    }
+
+    public void setFiyat(Double fiyat) {
+        this.fiyat = fiyat;
+    }
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
 }
